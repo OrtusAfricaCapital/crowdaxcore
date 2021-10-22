271b5cfdd8cc0d00d2364ec6f1ce69628c510e4b
Jjingo Kisakye <sseguku@gmail.com>
2021-10-22 15:06:08 +0300

    fix routes

--- app/controllers/api/v1/ventures_controller.rb
+++ app/controllers/api/v1/ventures_controller.rb
@@ -1,50 +1,54 @@
-class Api::V1::VenturesController < ApplicationController
-  before_action :restrict_access
-  #retrieves one venture 
-  def show
-    venture = Venture.find(params[:id])
-    render json:{venture: venture}
-  end
+module Api
+  module V1
+    class VenturesController < ApplicationController
+      before_action :restrict_access
+      #retrieves one venture 
+      def show
+        venture = Venture.find(params[:id])
+        render json:{venture: venture}
+      end
 
-  #show all ventures
+      #show all ventures
 
-  def index 
-    ventures = Venture.order('created_at DESC')#.where(approval_status: "false")
-    render json:{ventures: ventures}
-  end
+      def index 
+        ventures = Venture.order('created_at DESC')#.where(approval_status: "false")
+        render json:{ventures: ventures}
+      end
 
-  #creates a new venture 
-  def create
-     venture = Venture.new(venture_params)
+      #creates a new venture 
+      def create
+        venture = Venture.new(venture_params)
 
-     if venture.save 
-        render json: {venture: venture}, status: :created
-     else
-        render json: {status: 'Error', message: 'Venture not saved', data: venture.errors}, status: :unprocessable_entity
-     end
-  end
+        if venture.save 
+            render json: {venture: venture}, status: :created
+        else
+            render json: {status: 'Error', message: 'Venture not saved', data: venture.errors}, status: :unprocessable_entity
+        end
+      end
 
-  def destroy
-    venture = Venture.find(params[:id])
-    venture.destroy
-    render json: {status: 'SUCCESS', message: 'Deleted venture', data: venture}, status: :ok
-  end
+      def destroy
+        venture = Venture.find(params[:id])
+        venture.destroy
+        render json: {status: 'SUCCESS', message: 'Deleted venture', data: venture}, status: :ok
+      end
 
-  def update
-    venture = Venture.find(params[:id])
-    if venture.update(venture_params)
-      render json: {venture: venture}
-    else
-      render json: {status: 'Error', message: 'Venture not updated', data: venture.errors}, status: :unprocessable_entity
+      def update
+        venture = Venture.find(params[:id])
+        if venture.update(venture_params)
+          render json: {venture: venture}
+        else
+          render json: {status: 'Error', message: 'Venture not updated', data: venture.errors}, status: :unprocessable_entity
+        end
+      end
+
+
+
+      private
+
+      def venture_params
+        params.permit(:user_id, :name, :short_description, :logo, :sector, :investment_stage, :traction, :location, :referred_by, :pitchdeck)
+        # params.permit(:user_id, :name, :short_description,  :sector, :investment_stage)
+      end
     end
-  end
-
-
-
-  private
-
-  def venture_params
-    params.permit(:user_id, :name, :short_description, :logo, :sector, :investment_stage, :traction, :location, :referred_by, :pitchdeck)
-    # params.permit(:user_id, :name, :short_description,  :sector, :investment_stage)
   end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,5 +6,5 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins "https://crowdax.herokuapp.com"
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-   end
+    end
    end
