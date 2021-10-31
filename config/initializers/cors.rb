6af5484ebf23df32485952d74aa1f64ae4592702
Jjingo Kisakye <sseguku@gmail.com>
2021-10-31 16:32:18 +0300

    testing allow all

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,4 +7,14 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       origins "https://crowdax.herokuapp.com"
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
+
+    allow do
+      origins "https://crowdaxxx.herokuapp.com"
+      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
+
+    allow do
+      origins "*"
+      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
    end
