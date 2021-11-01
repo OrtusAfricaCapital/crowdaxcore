ebc5f7108f686a287ae371077504ca3ddb70e684
Jjingo Kisakye <sseguku@gmail.com>
2021-11-01 14:29:49 +0300

    cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     end
 
     allow do
-      origins "https://crowdaxxx.herokuapp.com"
+      origins "http://localhost:3000"
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
    end
