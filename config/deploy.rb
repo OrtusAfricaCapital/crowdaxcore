da1265d4cb14439b205817677ead7be69f2ba8ca
Jjingo Kisakye <sseguku@gmail.com>
2021-11-14 02:51:08 +0300

    update server details

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
-set :application, "crowdaxcore"
+set :application, "crowdax"
 set :repo_url, "git@github.com:OrtusAfricaCapital/crowdaxcore.git"
 
 # Deploy to the user's home directory
-set :deploy_to, "/home/crowdax/#{fetch :application}"
+set :deploy_to, "/home/#{fetch :application}"
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
