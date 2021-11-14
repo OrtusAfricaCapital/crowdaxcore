8a0de3264c69c546778ab4378d9bb04c71592893
Jjingo Kisakye <sseguku@gmail.com>
2021-11-15 02:48:01 +0300

    updated deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "crowdax"
 set :repo_url, "git@github.com:OrtusAfricaCapital/crowdaxcore.git"
 
 # Deploy to the user's home directory
-set :deploy_to, "/home/#{fetch :application}"
+set :deploy_to, "/home/crowdax/#{fetch :application}"
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
