7a3a96c9eb95776feef23024b501d92011cf6d6d
Jjingo Kisakye <sseguku@gmail.com>
2022-03-09 13:20:41 +0300

    added crowdax.co to cors

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 if Rails.env == "production"
 
-    Rails.application.config.session_store :cookie_store, key: "_crowdax_api", domain: "https://crowdax.herokuapp.com"
+    Rails.application.config.session_store :cookie_store, key: "_crowdax_api", domain: "https://crowdax.co"
     else
         Rails.application.config.session_store :cookie_store, key: "_crowdax_api"
     
